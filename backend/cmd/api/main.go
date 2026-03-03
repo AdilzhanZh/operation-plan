b9e7f98b96464c11c1ad292b8b48d6bc2c738ff5
Adilzhan <adilzhanzhanabil0705@gmail.com>
2026-03-03 14:54:02 +0500

    adding config-logger-server layers

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"OperationPlan/internal/config"
+	"OperationPlan/internal/pkg/logger"
+	"OperationPlan/internal/server"
+	"log"
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %s", err.Error())
+	}
+
+	slogger := logger.New(cfg.LogLevel)
+	slog.SetDefault(slogger)
+
+	r := gin.New()
+
+	r.GET("/test", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"message": "It's all right"})
+	})
+
+	srv := server.New(r, cfg.Port)
+	srv.Run()
 }
--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,1 +1,25 @@
 package server
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type Server struct {
+	httpServ *http.Server
+}
+
+func New(router http.Handler, port string) *Server {
+	return &Server{
+		httpServ: &http.Server{
+			Addr:    ":" + port,
+			Handler: router,
+			//TODO configs
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	slog.Info("server is started", "ADDR", s.httpServ.Addr)
+	return s.httpServ.ListenAndServe()
+}
